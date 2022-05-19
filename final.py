3fcc7f1e238b65ed4b6556ea54914cb9cfa9cff2
Unique Orduna <uniqueorduna@swuser-129-8-207-12.swlan.csufresno.edu>
2022-05-19 16:07:03 -0700

    finish

---
+++ final.py
@@ -0,0 +1,21 @@
+"""
+The program is looking to create a function that will calculate the perecent of grades
+that are above that average score
+The program is going to open file "final(1).txt"
+It will then calculate the percentage of grades that are above average
+it will gather all the grades from the file "final(1).txt"
+it will add all grades and then calculate the average grade score
+it will return the count and multiply the count by 100 then divide by the count of grades
+
+The program will define a main function to kick start the next series 
+it wil open the file and set grades to a empty list which will be imported from the file "final(1).txt"
+it will append the grades and then print the number of grades
+next the average is set to 0 so that it can be calculated
+the average score is then divided by the count of grades to check how many grades are above average
+the percent of grades that are above average is then calculated
+print to the user the average grade score
+print to the user the percent of grades that are above average
+close the file 
+
+
+"""
